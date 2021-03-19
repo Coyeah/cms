001ad0996f62b588e874d662c50ae7c4711cfbaf
coyeah <Coyeah_chen@outlook.com>
2021-03-19 11:46:40 +0800

    feat: add client and can build

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,8 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import initApolloServer from "./construct/initApolloServer";
 import initConnection from "./construct/initConnection";
@@ -9,7 +10,6 @@ import initConnection from "./construct/initConnection";
 const PORT = 3001;
 
 async function bootstrap() {
-
   // 初始化 orm
   await initConnection();
 
@@ -18,20 +18,18 @@ async function bootstrap() {
   // utils
   app.use(bodyParser.json());
   app.use(cookieParser());
+  app.use(cors());  // 全局允许跨域
 
   // apollo-server-express
   const apolloServer = await initApolloServer();
   apolloServer.applyMiddleware({ app });
 
-  app.get("/ok", function (req: Request, res: Response) {
-    res.send("ok");
-  });
-
   app.listen(PORT, () => {
     console.log(`Running app listening at: http://localhost:${PORT}`);
-    console.log(`GraphQL Playground available at: http://localhost:${PORT}/graphql`)
+    console.log(
+      `GraphQL Playground available at: http://localhost:${PORT}/graphql`
+    );
   });
-
 }
 
 bootstrap();
